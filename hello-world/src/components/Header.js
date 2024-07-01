344d5d3f241fbc30626a17f70e429cae709a6925
vikasg-16 <vikasgupta16699@gmail.com>
2024-07-01 22:03:18 +0530

    .

--- hello-world/src/components/Header.js
+++ hello-world/src/components/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 const Header = ( ) =>{
     return (
        <div className="ui fixed menu">
-        <div className="ui container left">
-         <h2>Conatact Manager</h2>   
-        </div>
+            <div className="ui container center">
+                <h2>Conatact Manager</h2>   
+            </div>
         </div>
     );
 };
